485b24a49d0af0ec2460d28a8d5b9f07cd88fdf1
Eduardo <eduardo.barbosa@performait.com>
2024-09-04 21:44:12 -0300

    estruturando o inicio do projeto

---
+++ src/app/components/internal/internal.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-internal',
+  templateUrl: './internal.component.html',
+  styleUrls: ['./internal.component.scss'],
+  standalone: true
+})
+export class InternalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/core/core.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { initialRoutes } from '../components/initial/initial.routes';
+
+export const coreRoutes: Routes = [
+  ...initialRoutes
+];
